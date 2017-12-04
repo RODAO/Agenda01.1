cb1cb54df6f9e021c3a4006f099bca38a44c5005
CELSO VIEIRA <celsorodado@gmail.com>
2017-12-04 17:08:34 -0200

    alteraçao git

--- app/src/main/java/br/com/alura/agenda01/FormularioActivity.java
+++ app/src/main/java/br/com/alura/agenda01/FormularioActivity.java
@@ -121,7 +121,7 @@ public class FormularioActivity extends AppCompatActivity {
                     dao.insere(aluno);
                 }
                 dao.close();
-
+ // teste git
                 Toast.makeText(FormularioActivity.this, "Aluno" + aluno.getNome() + "salvo!", Toast.LENGTH_SHORT).show();
                 finish();
                 break;
